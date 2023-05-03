566f791ce98f56c32e4af0cafa2eddb27a85f48a
SirJowi <johann-sinner@web.de>
2023-05-03 20:24:18 +0200

    + comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-""" Programm zur Ermittlung der Versagenswahrscheinlichkeit eines statischen Systems
-    im Zuge einer Übungsaufgabe
+""" Programm zur Ermittlung der Versagenswahrscheinlichkeit eines statischen Systems im Zuge einer Übungsaufgabe
     03/05/23 """
 
 import numpy as np
@@ -7,7 +6,7 @@ import math
 from scipy.stats import norm
 import scipy.integrate as integrate
 
-# Last G (Normalverteilung) ----------------------------------------------------------------------
+# Last G (Normalverteilung) --------------------------------------------------------------------------------------------
 my_x1 = 230.0     # Erwartungswert der Last G in [kN]
 sigma_x1 = 50.0   # Standardabweichung der Last G in [kN]
 
@@ -16,7 +15,7 @@ def f1(x1):      # Wahrscheinlichkeitsdichtefunktion
     return 1 / np.sqrt(2 * math.pi * sigma_x1 ** 2) * np.exp(-1/2 * ((x1 - my_x1)/sigma_x1)**2)
 
 
-# Festigkeit f_y (Logarithmisch Normalverteilt) --------------------------------------------------
+# Festigkeit f_y (Logarithmisch Normalverteilt) ------------------------------------------------------------------------
 my_x2 = 25.7 * 10**4    # Erwartungswert der Festigkeit in [kN/m^2]
 sigma_x2 = 2.01 * 10**4     # Standardabweichung der Festigkeit [kN/m^2]
 x_02 = 19.95 * 10**4    # [kN/m^2]
@@ -35,16 +34,15 @@ def SNV(x2):
     return val
 
 
-# Grenzzustandsfunktion -------------------------------------------------------------------------
-A = 1.44 * 10**-4   # [m^2] Querschnittsfläche
+# Grenzzustandsfunktion ------------------------------------------------------------------------------------------------
+A = 0.014   # [m^2] Querschnittsfläche
 W_el = 1124 * 10**-6    # [m^3]
 W_pl = 1350 * 10**-6    # [m^3]
 C1_el = -1 * ((29/97 * np.sin(np.arctan(5.2/3.9))) / A + (29/97 * 3.9) / W_el)     # Grenzzustand elastisch
 C1_pl = -1 * ((29/97 * 3.9) / W_pl)    # Grenzzustand plastisch
 
-# Versagenswahrscheinlichkeit ------------------------------------------------------------------
 
-
+# Versagenswahrscheinlichkeit ------------------------------------------------------------------------------------------
 def P_f(x_1):
     x_2 = - C1_el * x_1
     return f1(x_1) * SNV(x_2)
@@ -52,7 +50,7 @@ def P_f(x_1):
 
 P_f_int = integrate.quad(P_f, -x_02/C1_el, 10**5)
 
-# Ausgabe -------------------------------------------------------------------------------------
+# Ausgabe --------------------------------------------------------------------------------------------------------------
 print("sigma:", sigma_u)
 print("my:", my_u)
 print("C1_el:", C1_el)
